0f49d18a771b540da32c753cf062b2963f68f23f
Miracl3nb <109076069+Miracl3nb@users.noreply.github.com>
2023-07-12 15:29:55 -0300

    trabajo en front

--- app.js
+++ app.js
@@ -1,23 +1,47 @@
 //alerts
-function alertaInicial(){
+function alertaInicial() {
+  swal({
+    title: "URGENCIAS",
+    text: "comunicate al 11 55258145",
+    icon: "warning",
+  });
+}
+
+function envioExitoso() {
     swal({
-        title: 'URGENCIAS',
-        text: 'comunicate al 11 55258145',
-        icon: "warning"
+        title: "Excelente",
+        text: "Mensaje Enviado",
+        icon: "success",
     });
 
-}
 
-alertaInicial()
-
-
-function envioExitoso(){
+function faltandatos(){
     swal({
-        title: 'Excelente',
-        text: 'Mensaje Enviado',
-        icon: 'success'
+        title: 'DATOS ERRONEOS',
+        Text: 'Esta ingresando datos erroneos o dejando casilleros vacios',
+        icon: 'error'
     })
 }
+
+
+}
+
+function validarFormulario(){
+    let nombre = document.getElementById('nombre').value
+    let correo = document.getElementById('correo').value
+    let mensaje = document.getElementById('mensaje').value
+
+
+
+    if (mensaje =='' || correo ==''|| nombre ==''){
+        return faltandatos()
+    }else{
+        return envioExitoso()
+    }
+
+}
+  // Obtener los valores de los campos
+
 
 
 //alertaInicial()
