78db01d34448c565ca38b8dcedbcd873c387bbc0
Miracl3nb <109076069+Miracl3nb@users.noreply.github.com>
2023-07-17 21:43:38 -0300

    aplicando sender mail

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from formmail import envio_mail
 
 app = Flask(__name__)
 
@@ -8,6 +8,31 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/contacto', method=['POST'])
+def contacto():
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
+
+
+#funcion de request asiganacion varialble
+
+    """
+    def procesar(nombre, correo, texto):
+    nombre = request.form[ 'nombre' ]
+    correo = request.form[ 'correo' ]
+    texto  = request.form[ 'texto' ]
+    
+    salida = print(f" nombre = {nombre}, correo = {correo}, texto = {texto}")
+    
+    return salida
+    
+    
+    
+    
+    
+    
+    
+    """
--- app/formmail.py
+++ app/formmail.py
@@ -1,3 +1,35 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def envio_mail():
+    #remitente
+    REMITENTE = 'contacto.miracl3dev@gmail.com'
+    KEY ='lscuaezfrvcieoqv'
+    #destinatario
+    DESTINATARIO = 'miracl3nb@gmail.com'
+    ASUNTO = 'Nueva solitidud de contacto'
+
+    #mesaje
+
+    correo = MIMEMultipart()
+    correo['From'] = REMITENTE
+    correo['To'] = DESTINATARIO
+    correo['Subjet'] = ASUNTO
+
+    #cuerpo del mensaje
+
+    cuerpo = 'este mail es de prueba'
+    correo.attach(MIMEText(cuerpo, 'plain'))
+
+    #sesion serv mail
+
+    SERVER = smtplib.SMTP('smtp.gmail.com', 587)
+    SERVER.starttls()
+    SERVER.login(REMITENTE, KEY)
 
 
+    texto = correo.as_string()
+    SERVER.sendmail(REMITENTE, DESTINATARIO, texto)
+    SERVER.quit()
 
